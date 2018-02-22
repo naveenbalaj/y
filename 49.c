224f3bcdcfc44be26712661c2dfadb7e078fac63
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-02-22 16:05:53 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c;
+printf("addition");
+printf("1st number");
+scanf("%d",&a);
+printf("2st number");
+scanf("%d",&b);
+c=a+b;
+printf("%d",c);
+
+    return 0;
+}
+
